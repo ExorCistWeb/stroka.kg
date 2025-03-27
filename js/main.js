da941fd0b9fca9c4f82f3481619d36f7011182c9
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2025-03-27 08:57:34 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ document.getElementById('priceButton').addEventListener('click', function() {
     this.style.display = 'none';
     document.querySelector('.price_input').style.display = 'flex';
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const profileBtn = document.querySelector(".profile-btn");
+    const modal = document.querySelector(".lk_modal");
+
+    profileBtn.addEventListener("click", function() {
+        modal.classList.toggle("active");
+    });
+});
