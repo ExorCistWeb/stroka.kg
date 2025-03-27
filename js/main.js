6bbbc393db00a1185a8580ad338c1c03f1f4a4d6
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2025-03-27 07:32:43 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 const currentYear = new Date().getFullYear();
 document.querySelector('.date').textContent = currentYear;
 //  Год в футере
+
+// Цена в фильтре
+document.getElementById('priceButton').addEventListener('click', function() {
+
+    this.style.display = 'none';
+    document.querySelector('.price_input').style.display = 'flex';
+});
